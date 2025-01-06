a37948915f700dd7f9d180442465f7947552fe5c
Martin Řehořek <martinrehorek5@gmail.com>
2025-01-06 23:08:39 +0100

    User management for admins

--- src/main/java/cz/uhk/kppro/controller/indexController.java
+++ src/main/java/cz/uhk/kppro/controller/indexController.java
@@ -4,12 +4,36 @@ import jakarta.persistence.OneToMany;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+import cz.uhk.kppro.model.User;
+import cz.uhk.kppro.service.UserService;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Controller
 public class indexController {
 
+    private final UserService userService;
+
+    public indexController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping({"/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user;
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+            user = userService.findByUsername(username);
+        } else {
+            // Handle the case where the user is not authenticated
+            user = null;
+        }
+
+        model.addAttribute("user", user);
         return "index";
     }
 
--- src/main/java/cz/uhk/kppro/security/SecurityConfig.java
+++ src/main/java/cz/uhk/kppro/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/users/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
